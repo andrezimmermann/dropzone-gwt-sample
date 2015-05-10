6569f4b6c06b39917efc42148ec0071a95ed2cdf
André Zimmermann <andrezimmermann@email.com>
2015-05-10 19:56:16 -0300

    Reedited the Sample project.

---
+++ src/main/java/com/twocentsforthoughts/dropzone/server/FileUploadServlet.java
@@ -0,0 +1,18 @@
+package com.twocentsforthoughts.dropzone.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FileUploadServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Here you should read the data sent by the client
+        //This is just a mock
+        resp.setStatus(200);
+
+    }
+}
